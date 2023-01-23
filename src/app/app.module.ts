8c4c5f5b7a4c869f73ad997df08d3125de81ba97
deo002 <oberoidearsh@gmail.com>
2023-01-23 20:24:41 +0530

    Add welcome page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomePageComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
